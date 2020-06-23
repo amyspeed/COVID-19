1cc0ea5fd6d3c9bcd5cd1976906813b06b0d96cc
Amy Speed <amy.speed.henley@gmail.com>
2020-06-23 02:30:13 -0500

    test cors in production

--- src/actions/news.js
+++ src/actions/news.js
@@ -1,4 +1,4 @@
-import { NEWS_BASE_URL } from '../config';
+import { NEWS_BASE_URL, CORS_PROXY } from '../config';
 import normalizeResponseErrors from './utils';
 
 export const GET_NEWS_SUCCESS = 'GET_NEWS_SUCCESS';
@@ -14,9 +14,8 @@ export const getNewsError = error => ({
 });
 
 export const fetchNews = () => (dispatch) => {
-    return fetch(`${NEWS_BASE_URL}?q=COVID&language=en&sortBy=publishedAt&pageSize=6&page=1`, {
+    return fetch(`${CORS_PROXY + NEWS_BASE_URL}?q=COVID&language=en&sortBy=publishedAt&pageSize=6&page=1`, {
         method: 'GET',
-        mode: 'no-cors',
         headers: { 
             Authorization: 'Bearer 9d728f0139044cf3a54a15e546d1851e',
         },
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const CORONA_NINJA_BASE_URL = 'https://corona.lmao.ninja/v2';
 export const NEWS_BASE_URL = 'https://newsapi.org/v2/top-headlines';
-// export const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+export const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
